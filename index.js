fc8a9a58b9455f5ecd6520859dca93498e6dcbab
Aida Dieudonne <aidaharing10@gmail.com>
2024-10-10 19:04:08 -0600

    added a button and event listener

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const button = document.getElementById("contactme");
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
 
-button.addEventListener("click", 
-    function()
-);
+myButton.addEventListener("click", event => {
+    
+    if(myImg.style.visibility === "hidden"){
+        myImg.style.visibility = "visible";
+        myButton.textContent = "Hide";
+    }
+    else{
+        myImg.style.visibility = "hidden";
+        myButton.textContent= "Show";
+    }
+});
